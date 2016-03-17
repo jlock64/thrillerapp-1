a9a61c728eaa059e90a8a43c943756d2ed11541a
drewmunnerlyn <drewmunnerlyn@gmail.com>
2016-03-17 13:30:06 -0400

    intial commit to mothership

---
+++ src/main/java/com/theironyard/ThrillerAppApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThrillerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThrillerAppApplication.class, args);
+	}
+}
